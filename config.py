358bd08a44e18cc9a0a924e80c89202bc71690a9
TheBlackXYZBotz <155623566+TheBlackxyz@users.noreply.github.com>
2024-08-07 09:10:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,19 +2,19 @@ import os, re
 
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+API_ID = int(os.environ.get("API_ID", "29450452"))
+API_HASH = os.environ.get("API_HASH", "54759945ff88b52777eec9a455944d31")
+OWNER = int(os.environ.get("OWNER", "1759982322"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "TheBlackXYZRenameBot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "TheBlackXYZ")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "TheBlackXYZBotz")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002101130967"))
 
 DB_URL = os.environ.get("DB_URL", "")
 DB_NAME = os.environ.get("DB_NAME", "Cluster0")
 
-STRING = os.environ.get("STRING", "")
-BOT_PIC = os.environ.get("BOT_PIC", "")
+STRING = os.environ.get("STRING", "BQHBYNQAQT3ABkSSl_2L3K9hRL7nFlroIfpkC7lDIOaAOR8tImAbzxigaVzeY2GVVjY_RqPwYNBaypkbVfIH6l_GoEP80fDhd8wCoEWcb16rt2ZIgDfi5Jo3wTfb398Y8Iudc3qx0h2WgXvIGvS_QF-SuBXmSpeHcns9Ox0amyOf18Tmpju_4JG_wQeD8IJD8_UTClyhw1hwqO0oF6iwBx0swI8H4XvKxyGYSvNGGswxqSpJW4JJdAXJm1G8K5hxmNx6EQUAAV9QxduDWfjxjreq8wrWb78XsH5-eTqduAX3eCcqU9n7Yns9Gu45_h--JYOzudlS2WGTx29IUqoctIrODUJ9sgAAAAGEJs3oAQ")
+BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/9c7c198f925c3851838ce.jpg")
 
 
 
